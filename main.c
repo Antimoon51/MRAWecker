c4715e622e6e6b6f315041582674624a4f331df7
Antimoon51 <50957040+Antimoon51@users.noreply.github.com>
2019-06-17 17:51:57 +0200

    Added leapyear

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 void outputtime();
 void outputdate();
 void timeCorrection();
-
+char schaltjahr();
 typedef struct
 {
     uint8_t sec;
@@ -16,15 +16,10 @@ typedef struct
     uint8_t day;
     uint8_t mon;
     uint16_t year;
+    char weekday;
 } time_t;
 
 time_t time;
-
-
-
-
-
-
 
 int main(void)
 {
@@ -35,9 +30,9 @@ int main(void)
 
     time.sec = 55;
     time.min = 59;
-    time.hour = 9;
-    time.day = 1;
-    time.mon = 1;
+    time.hour = 23;
+    time.day = 28;
+    time.mon = 2;
     time.year = 2019;
 
     __enable_interrupt();
@@ -52,75 +47,95 @@ int main(void)
         __low_power_mode_3();
         timeCorrection();
 
+
     }
 
 }
 
-void outputtime(){
-    lcd_gotoxy(4,0);
-    lcd_put_char((time.hour - (time.hour % 10))/10 + 48);
+void outputtime()
+{
+    lcd_gotoxy(4, 0);
+    lcd_put_char((time.hour - (time.hour % 10)) / 10 + 48);
     lcd_put_char((time.hour % 10) + 48);
     lcd_write(":");
-    lcd_put_char((time.min - (time.min % 10))/10 + 48);
+    lcd_put_char((time.min - (time.min % 10)) / 10 + 48);
     lcd_put_char((time.min % 10) + 48);
     lcd_write(":");
-    lcd_put_char((time.sec - (time.sec % 10))/10 + 48);
+    lcd_put_char((time.sec - (time.sec % 10)) / 10 + 48);
     lcd_put_char((time.sec % 10) + 48);
 }
 
-void timeCorrection(){
+void timeCorrection()
+{
     if (time.sec > 59)
-       {
-           time.sec = 0;
-           time.min++;
+    {
+        time.sec = 0;
+        time.min++;
 
-           if (time.min > 59)
-           {
-               time.min = 0;
-               time.hour++;
+        if (time.min > 59)
+        {
+            time.min = 0;
+            time.hour++;
 
-               if (time.hour > 23)
-               {
-                   time.hour = 0;
-                   time.day++;
+            if (time.hour > 23)
+            {
+                time.hour = 0;
+                time.day++;
 
-                   if (time.day > 31
-                           & (time.mon == 1 || time.mon == 3 || time.mon == 5
-                                   || time.mon == 7 || time.mon == 8
-                                   || time.mon == 10 || time.mon == 12))
-                   {
-                       time.day = 1;
-                       time.mon++;
-                   }
-                   if (time.day > 30
-                           & (time.mon == 4 || time.mon == 6 || time.mon == 9
-                                   || time.mon == 11))
-                   {
-                       time.day = 1;
-                       time.mon++;
-                   }
-                   if (time.day > 28 & time.mon == 2)
-                   {
-                       time.day = 1;
-                       time.mon++;
-                   }
-               }
-           }
-       }
+                if (time.day > 31
+                        && (time.mon == 1 || time.mon == 3 || time.mon == 5
+                                || time.mon == 7 || time.mon == 8
+                                || time.mon == 10 || time.mon == 12))
+                {
+                    time.day = 1;
+                    time.mon++;
+                }
+                else if (time.day > 30
+                        && (time.mon == 4 || time.mon == 6 || time.mon == 9
+                                || time.mon == 11))
+                {
+                    time.day = 1;
+                    time.mon++;
+                }
+                else if (time.mon == 2)
+                {
+                  if (time.year % 4 == 0 && time.year % 100 != 0 || time.year % 400 == 0)
+                {
+                        if (time.day > 29)
+                        {
+                            time.mon++;
+                            time.day = 1;
+                        }
+                    }
+                    else
+                    {
+                        if (time.day > 28)
+                        {
+                            time.mon++;
+                            time.day = 1;
+                        }
+                    }
+
+                }
+            }
+        }
+    }
 }
 
-void outputdate(){
-        lcd_gotoxy(3,1);
-        lcd_put_char((time.day - (time.day % 10))/10 + 48);
-        lcd_put_char((time.day % 10) + 48);
-        lcd_write(".");
-        lcd_put_char((time.mon - (time.mon % 10))/10 + 48);
-        lcd_put_char((time.mon % 10) + 48);
-        lcd_write(".");
 
-        char Jahr[5];
-        snprintf(Jahr, sizeof(Jahr), "%d",time.year);
-        lcd_write(Jahr);
+void outputdate()
+{
+    lcd_gotoxy(3, 1);
+    lcd_put_char((time.day - (time.day % 10)) / 10 + 48);
+    lcd_put_char((time.day % 10) + 48);
+    lcd_write(".");
+    lcd_put_char((time.mon - (time.mon % 10)) / 10 + 48);
+    lcd_put_char((time.mon % 10) + 48);
+    lcd_write(".");
+
+    char Jahr[5];
+    snprintf(Jahr, sizeof(Jahr), "%d", time.year);
+    lcd_write(Jahr);
 }
 
 #pragma vector=WDT_VECTOR
